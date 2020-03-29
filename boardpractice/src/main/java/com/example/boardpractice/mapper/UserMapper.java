f025eca033bbb13d5d82df424e581ab6e043fbb1
DaEunShim <10ekdms01@naver.com>
2020-03-29 22:33:28 +0900

    [Post] 회원가입 mapper 기능 구현 완료

--- boardpractice/src/main/java/com/example/boardpractice/mapper/UserMapper.java
+++ boardpractice/src/main/java/com/example/boardpractice/mapper/UserMapper.java
@@ -1,4 +1,17 @@
 package com.example.boardpractice.mapper;
 
+import com.example.boardpractice.dto.User;
+import com.example.boardpractice.model.SignupReq;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
 public interface UserMapper {
+
+    @Insert("INSERT INTO User(userName, userId, userPw) VALUES(#{signupReq.userName},#{signupReq.userId},#{signupReq.userPw})")
+    // @Options(useGeneratedKeys = true, keyColumn = "user.userIdx")
+    int signup(@Param("signupReq") final SignupReq signupReq);
+
 }
