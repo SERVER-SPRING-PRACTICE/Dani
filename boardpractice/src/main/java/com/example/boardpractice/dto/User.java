6d8944c20e076192b89ea25628ae1639f4987978
DaEunShim <10ekdms01@naver.com>
2020-03-29 22:33:52 +0900

    [Post] 회원가입 dto 기능 구현 완료

--- boardpractice/src/main/java/com/example/boardpractice/dto/User.java
+++ boardpractice/src/main/java/com/example/boardpractice/dto/User.java
@@ -1,4 +1,15 @@
 package com.example.boardpractice.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+// 서버 -> 클라이언트
 public class User {
+    private int userIdx;
+    private String userName;
+    private String userId;
 }
