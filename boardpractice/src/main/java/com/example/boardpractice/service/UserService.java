dfe3eb3f7da46592f6670e76c3de30f59b7db366
DaEunShim <10ekdms01@naver.com>
2020-03-29 22:32:22 +0900

    [Post] 회원가입 service 기능 구현 완료

--- boardpractice/src/main/java/com/example/boardpractice/service/UserService.java
+++ boardpractice/src/main/java/com/example/boardpractice/service/UserService.java
@@ -1,4 +1,29 @@
 package com.example.boardpractice.service;
 
+import com.example.boardpractice.dto.User;
+import com.example.boardpractice.mapper.UserMapper;
+import com.example.boardpractice.model.DefaultRes;
+import com.example.boardpractice.model.SignupReq;
+import com.example.boardpractice.utils.ResponseMessage;
+import com.example.boardpractice.utils.StatusCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class UserService {
+
+    private final UserMapper userMapper;
+
+    public UserService(final UserMapper userMapper){ this.userMapper = userMapper;}
+
+    public DefaultRes save(final SignupReq signupReq){
+        try{
+            userMapper.signup(signupReq);
+            return DefaultRes.res(StatusCode.CREATED,ResponseMessage.SIGN_UP_INSERT_SUCCESS);
+        } catch(Exception e){
+            log.error(e.getMessage());
+            return DefaultRes.res(StatusCode.DB_ERROR, ResponseMessage.DB_ERROR);
+        }
+    }
 }
